1bae457bb6bf6fd56556490faa76096c38e81cef
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-01-12 19:02:22 -0500

    Added constructor logic to class

--- RationalNumber/src/RationalNumber.java
+++ RationalNumber/src/RationalNumber.java
@@ -1,8 +1,30 @@
 
 public class RationalNumber {
 
-	public static void main(String[] args) {
-		System.out.println("test");
+	int numerator,
+		denominator;
+
+	public RationalNumber() {
+		numerator = 0;
+		denominator = 1;
+	}
+
+	public RationalNumber(int num, int den) {
+
+		if (den < 0) {
+			denominator = -den;
+
+			if (num < 0) {
+				numerator = num;
+			} else {
+				numerator = -num;
+			}
+		
+		} else if (den > 0) {
+			denominator = den;
+			numerator = num;
+		}
+
 	}
 
 }
