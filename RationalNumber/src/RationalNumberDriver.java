a8b308654724a6ce6d2f4677993fe5a456f66f16
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2019-01-18 08:03:55 -0500

    Update Rational number driver

--- RationalNumber/src/RationalNumberDriver.java
+++ RationalNumber/src/RationalNumberDriver.java
@@ -4,45 +4,13 @@ public class RationalNumberDriver {
 	public static void main(String[] args) {
 
 		Boolean test;
+		
+		RationalNumber rat = new RationalNumber(3,2);
+		
+		RationalNumber r = new RationalNumber(2,3);
+		
+		RationalNumber e = rat.add(r);
+		System.out.println(e.toString());
 	}
 
-}
-
-
-
-public class testcircle {
-	public static void main(String[] args) {
-		Boolean test;
-		
-		Circle cir = new Circle(1243.6);
-		System.out.println(cir.getRadius());
-		System.out.println(cir.getDiameter());
-		System.out.println(cir.getCircumference());
-		System.out.println(cir.getArea());
-		System.out.println(cir.toString());
-		Circle circle2 = new Circle(cir);
-//		circle2 = cir.copy();
-		System.out.println(circle2.getRadius());
-		System.out.println(circle2.getDiameter());
-		System.out.println(circle2.getCircumference());
-		System.out.println(circle2.getArea());
-		System.out.println(circle2.toString());
-		
-		test=cir.equals(circle2);
-		System.out.println(test);
-		
-		System.out.println(cir.getRadius());
-		System.out.println(cir.getDiameter());
-		System.out.println(cir.getCircumference());
-		System.out.println(cir.getArea());
-		System.out.println(cir.toString());
-		
-		test=cir.equals(circle2);
-		System.out.println(test);
-		
-		System.out.println(circle2.getRadius());
-		System.out.println(circle2.getDiameter());
-		System.out.println(circle2.getCircumference());
-		System.out.println(circle2.getArea());
-	}
 }
