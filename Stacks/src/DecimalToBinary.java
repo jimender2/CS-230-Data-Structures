8af1bee27d770f54f482f8b67a0a5d2047521f0c
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2019-02-06 08:50:58 -0500

    update decimal to binary driver

--- Stacks/src/DecimalToBinary.java
+++ Stacks/src/DecimalToBinary.java
@@ -1,10 +1,40 @@
-
+import java.util.Scanner;
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
 
+		int x;
+		Scanner keyboard = new Scanner( System.in);
 
+		for( int count = 1 ; count <= 5 ; count++) {
+			System.out.println( "Enter a postitive integer: ");
+			x = keyboard.nextInt();
+			if( x >= 0)
+				convert( x);
+			else
+				System.out.println( x + " is negative.");
+		}
 
+		keyboard.close();
+		return;
+	}
+
+	public static void convert( int num) {
+
+		int rem;
+		ABStack<Integer> stack = new ABStack<Integer>();
+
+		while( num > 0) {
+			rem = num % 2;
+			stack.push( rem);
+			num = num / 2;
+		}
+		
+		while( !stack.isEmpty())
+			System.out.print( stack.pop());
+
+		System.out.println();
+		return;
 	}
 
 }
