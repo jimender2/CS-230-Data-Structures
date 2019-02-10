183b849d81bbce908fcf3a11575b64c687fa45ad
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-02-09 21:49:58 -0500

    update auction driver

--- TrackingOnlineAuction/src/TrackingOnlineAuction.java
+++ TrackingOnlineAuction/src/TrackingOnlineAuction.java
@@ -2,11 +2,40 @@
  * @author Jonathan Meredith
  *
  */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 public class TrackingOnlineAuction {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
-		int test;
+		File fileIn = new File("bids.txt");
+		Scanner file = new Scanner(fileIn);
+
+		ABStack person = new ABStack<String>();
+		ABStack bid = new ABStack<Integer>();
+
+		int tempBid,
+			maxBid,
+			highBidder;
+
+		String tempPerson,
+				out;
+
+		System.out.println("New Bid      Result      High      Bidder      High Bid      Maximum Bid");
+		System.out.println("-------------------------------------------------------------------------------");
+		while(file.hasNext()) {
+			tempBid = file.nextInt();
+			tempPerson = file.nextLine();
+			
+		}
+		
+		while (!bid.isEmpty()) {
+			System.out.println(person.pop() + " " + bid.pop());
+		}
+
+		file.close();
 	}
 
 }
