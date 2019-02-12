fe06ef75752d9153d745f800fb8439946f8ac100
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-02-12 16:55:42 -0500

    Updates to Assignment 3 for comparing bids

--- TrackingOnlineAuction/src/TrackingOnlineAuction.java
+++ TrackingOnlineAuction/src/TrackingOnlineAuction.java
@@ -17,18 +17,34 @@ public class TrackingOnlineAuction {
 		ABStack bid = new ABStack<Integer>();
 
 		int tempBid,
-			maxBid,
-			highBidder;
+			currentBid,
+			maxBid;
 
 		String tempPerson,
+				highBidder,
 				out;
 
-		System.out.println("New Bid      Result      High      Bidder      High Bid      Maximum Bid");
+		System.out.println("New Bid      Result      High Bidder           High Bid             Maximum Bid");
 		System.out.println("-------------------------------------------------------------------------------");
 		while(file.hasNext()) {
 			tempBid = file.nextInt();
 			tempPerson = file.nextLine();
-			
+			if (bid.isEmpty()) {
+				bid.push(1);
+				person.push(tempPerson);
+				highBidder = tempPerson;
+				currentBid = 1;
+				maxBid = tempBid;
+				System.out.println(tempBid + " " + tempPerson + "   New High Bidder  " + highBidder + "    " + currentBid + " " + maxBid);
+			} else if ( tempBid < (int) bid.peek() ) {
+				//Do nothing
+			} else if ( tempBid == (int) bid.peek()) {
+				currentBid = tempBid;
+				//Need to fix this part
+				//System.out.println(tempBid + " " + tempPerson + "   New High Bidder  " + highBidder + "    " + currentBid + " " + maxBid);
+			} else {
+				//Increase bid
+			}
 		}
 		
 		while (!bid.isEmpty()) {
