497140f89aa7d804be5f08bf5b9ac0635f6bb202
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-02-15 22:10:06 -0500

    Finished output of Assignment 3.

--- TrackingOnlineAuction/src/TrackingOnlineAuction.java
+++ TrackingOnlineAuction/src/TrackingOnlineAuction.java
@@ -1,11 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Jonathan Meredith
  *
  */
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
 public class TrackingOnlineAuction {
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -23,8 +24,10 @@ public class TrackingOnlineAuction {
 		String tempPerson,
 				highBidder = null;
 
-		System.out.println("New Bid      Result      High Bidder           High Bid             Maximum Bid");
-		System.out.println("-------------------------------------------------------------------------------");
+		System.out.printf("%10s %20s %20s %20s %20s", "New Bid", "Result", "High Bidder", "High Bid", "Maximum Bid");
+		System.out.println("");
+//		System.out.println("New Bid      Result      High Bidder           High Bid             Maximum Bid");
+		System.out.println("---------------------------------------------------------------------------------------------------------");
 		while(file.hasNext()) {
 			tempBid = file.nextInt();
 			tempPerson = file.nextLine();
@@ -34,7 +37,9 @@ public class TrackingOnlineAuction {
 				highBidder = tempPerson;
 				currentBid = 1;
 				maxBid = tempBid;
-				System.out.println(tempBid + " " + tempPerson + "   New High Bidder  " + highBidder + "    " + currentBid + " " + maxBid);
+				System.out.printf("%10s %20s %20s %20s %20s", tempBid + " " + tempPerson, "New High Bidder", person.peek(), currentBid, maxBid);
+				System.out.println("");
+//				System.out.println(tempBid + " " + tempPerson + "   New High Bidder  " + highBidder + "    " + currentBid + " " + maxBid);
 			} else if ( tempBid < currentBid ) {
 				//Do nothing
 				//works
@@ -43,11 +48,16 @@ public class TrackingOnlineAuction {
 				if (tempBid < maxBid) {
 					currentBid = tempBid;
 					if (tempBid == (int) bid.peek()) {
-						System.out.println(tempBid + " " + tempPerson + "   no change  " + person.peek() + "    " + currentBid + " " + maxBid);
+						System.out.printf("%10s %20s %20s %20s %20s", tempBid + " " + tempPerson, "No Change", person.peek(), currentBid, maxBid);
+						System.out.println("");
+//						System.out.println(tempBid + " " + tempPerson + "   No change  " + person.peek() + "    " + currentBid + " " + maxBid);
 					} else {
 						bid.push(currentBid);
 						person.push(person.peek());
-						System.out.println(tempBid + " " + tempPerson + "   high bid increased  " + person.peek() + "    " + currentBid + " " + maxBid);
+						System.out.printf("%10s %20s %20s %20s %20s", tempBid + " " + tempPerson, "High Bid Increased", person.peek(), currentBid, maxBid);
+						System.out.println("");
+
+//						System.out.println(tempBid + " " + tempPerson + "   High bid increased  " + person.peek() + "    " + currentBid + " " + maxBid);
 					}
 
 				} else if (tempBid > maxBid) {
@@ -55,19 +65,24 @@ public class TrackingOnlineAuction {
 					maxBid = tempBid;
 					bid.push(currentBid);
 					person.push(tempPerson);
-					System.out.println(tempBid + " " + tempPerson + "   new high bidder  " + person.peek() + "    " + currentBid + " " + maxBid);
+					System.out.printf("%10s %20s %20s %20s %20s", tempBid + " " + tempPerson, "New High Bidder", person.peek(), currentBid, maxBid);
+					System.out.println("");
+//					System.out.println(tempBid + " " + tempPerson + "   New high bidder  " + person.peek() + "    " + currentBid + " " + maxBid);
 
 				}
 
 			}
 		}
-		
-		System.out.println("");
-		while (!bid.isEmpty()) {
-			System.out.println(person.pop() + " " + bid.pop());
-		}
 
 		file.close();
+		System.out.println("");
+
+		System.out.println("The bid history for this auction is:");
+		while (!bid.isEmpty()) {
+			System.out.printf("%20s %s", person.pop(), bid.pop());
+			System.out.println("");
+		}
+
 	}
 
 }
