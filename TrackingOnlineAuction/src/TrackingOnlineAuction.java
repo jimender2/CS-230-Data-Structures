3c8242694e8e33e8fa85bf23d21014640734e2ca
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-02-15 22:24:06 -0500

    Final version of assignment 3.  With comments and styling.

--- TrackingOnlineAuction/src/TrackingOnlineAuction.java
+++ TrackingOnlineAuction/src/TrackingOnlineAuction.java
@@ -4,13 +4,15 @@ import java.util.*;
 
 /**
  * @author Jonathan Meredith
- *
+ * Assignment 3 for Data Structures
+ * Auction tracker
  */
 
 public class TrackingOnlineAuction {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
+		//Variables
 		File fileIn = new File("bids.txt");
 		Scanner file = new Scanner(fileIn);
 
@@ -21,65 +23,82 @@ public class TrackingOnlineAuction {
 			currentBid = 0,
 			maxBid = 0;
 
-		String tempPerson,
-				highBidder = null;
+		String tempPerson;
 
-		System.out.printf("%10s %20s %20s %20s %20s", "New Bid", "Result", "High Bidder", "High Bid", "Maximum Bid");
+		//Header
+		System.out.printf("%10s %20s %15s %10s %15s", "New Bid", "Result", 
+				"High Bidder", "High Bid", "Maximum Bid");
 		System.out.println("");
-//		System.out.println("New Bid      Result      High Bidder           High Bid             Maximum Bid");
-		System.out.println("---------------------------------------------------------------------------------------------------------");
+		System.out.println("---------------------------------------------"
+				+ "------------------------------");
+
+		//Read file in and do operations
 		while(file.hasNext()) {
 			tempBid = file.nextInt();
 			tempPerson = file.nextLine();
+
+			//Do checking if valid bid
 			if (bid.isEmpty()) {
+
 				bid.push(1);
 				person.push(tempPerson);
-				highBidder = tempPerson;
 				currentBid = 1;
 				maxBid = tempBid;
-				System.out.printf("%10s %20s %20s %20s %20s", tempBid + " " + tempPerson, "New High Bidder", person.peek(), currentBid, maxBid);
+				System.out.printf("%10s %20s %15s %10s %15s", tempBid + 
+						" " + tempPerson, "New High Bidder", 
+						person.peek(), currentBid, maxBid);
 				System.out.println("");
-//				System.out.println(tempBid + " " + tempPerson + "   New High Bidder  " + highBidder + "    " + currentBid + " " + maxBid);
+
 			} else if ( tempBid < currentBid ) {
+
 				//Do nothing
-				//works
+				//Reject bid because it is too small
+
 			} else if (tempBid >= currentBid) {
 
 				if (tempBid < maxBid) {
 					currentBid = tempBid;
+
 					if (tempBid == (int) bid.peek()) {
-						System.out.printf("%10s %20s %20s %20s %20s", tempBid + " " + tempPerson, "No Change", person.peek(), currentBid, maxBid);
+						System.out.printf("%10s %20s %15s %10s %15s", 
+								tempBid + " " + tempPerson, "No Change",
+								person.peek(), currentBid, maxBid);
 						System.out.println("");
-//						System.out.println(tempBid + " " + tempPerson + "   No change  " + person.peek() + "    " + currentBid + " " + maxBid);
+
 					} else {
 						bid.push(currentBid);
 						person.push(person.peek());
-						System.out.printf("%10s %20s %20s %20s %20s", tempBid + " " + tempPerson, "High Bid Increased", person.peek(), currentBid, maxBid);
+						System.out.printf("%10s %20s %15s %10s %15s", 
+								tempBid + " " + tempPerson, "High Bid "
+								+ "Increased", person.peek(), currentBid,
+								maxBid);
 						System.out.println("");
 
-//						System.out.println(tempBid + " " + tempPerson + "   High bid increased  " + person.peek() + "    " + currentBid + " " + maxBid);
 					}
 
 				} else if (tempBid > maxBid) {
+
 					currentBid = maxBid + 1;
 					maxBid = tempBid;
 					bid.push(currentBid);
 					person.push(tempPerson);
-					System.out.printf("%10s %20s %20s %20s %20s", tempBid + " " + tempPerson, "New High Bidder", person.peek(), currentBid, maxBid);
+					System.out.printf("%10s %20s %15s %10s %15s", tempBid 
+							+ " " + tempPerson, "New High Bidder", 
+							person.peek(), currentBid, maxBid);
 					System.out.println("");
-//					System.out.println(tempBid + " " + tempPerson + "   New high bidder  " + person.peek() + "    " + currentBid + " " + maxBid);
 
 				}
-
 			}
 		}
 
+		//Close file
 		file.close();
 		System.out.println("");
 
+		//Output results
 		System.out.println("The bid history for this auction is:");
 		while (!bid.isEmpty()) {
-			System.out.printf("%20s %s", person.pop(), bid.pop());
+			System.out.printf("%s %10s", person.pop(), bid.pop());
 			System.out.println("");
 		}
 
