1924a231bbfac5798008b8c233832bc3ff8a68fb
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2019-03-01 07:42:41 -0500

    Addition Problems Added

---
+++ Queues/src/SimpleAdditionProblems.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class SimpleAdditionProblems {
+	
+	public static void main(String[] args) {
+		
+		int operand1;
+		int operand2;
+		int studentAnswer;
+		int correctAnswers = 0;
+		int incorrectAnswers = 0;
+		Random rand = new Random();
+		final int MAX_PROBLEMS = 5;
+		Scanner keyboard = new Scanner( System.in);
+		
+		
+		for( int count = 1 ; count <= MAX_PROBLEMS ; count++)
+		{
+			
+		}
+		
+		
+		
+		System.out.println( "Done.");
+		keyboard.close();
+		return;
+	}
+
+}
