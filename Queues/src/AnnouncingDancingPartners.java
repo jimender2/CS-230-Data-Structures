20dc53caf556d6da4e236e2001fca96249606bc1
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2019-03-11 07:53:23 -0400

    Added queue files

---
+++ Queues/src/AnnouncingDancingPartners.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class AnnouncingDancingPartners {
+
+	public static void main(String[] args) throws IOException {
+
+		String dancer;
+		
+		Scanner infile = new Scanner( new File( "dancers.txt"));
+		
+		while( infile.hasNextLine()) {
+			
+			
+		}
+		
+		System.out.println( "Announcing Dancers: ");
+		
+		
+		
+		infile.close();
+		return;
+	}
+}
