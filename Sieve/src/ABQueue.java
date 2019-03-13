ccb9e9092dc68642394a859fd8139e49ddda6d79
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-03-13 07:39:56 -0400

    Worked on sieve

---
+++ Sieve/src/ABQueue.java
@@ -0,0 +1,105 @@
+
+public class ABQueue<T> {
+
+	private int back;
+	private int front;
+	private T [] queue;
+	
+	public ABQueue() {
+		
+		back = 0;
+		front = 0;
+		queue = (T[]) new Object[100];
+	}
+	
+	public ABQueue( int capacity) {
+		
+		back = 0;
+		front = 0;
+		if( capacity > 0)
+			queue = (T[]) new Object[capacity];
+		else
+			queue = (T[]) new Object[100];
+	}
+
+	public void enqueue( T element) {
+		
+		if( isFull()) {
+			T[] newQueue = (T[]) new Object[queue.length + 50];
+			for(int index = 0 ; index < back; index++)
+				newQueue[index] = queue[index];
+			queue = newQueue;
+		}
+		queue[back] = element;
+		back++;
+		return;		
+	}
+	
+	public T dequeue() {
+		
+		T item = null;
+		if( isEmpty()) {
+			System.out.println( "Empty queue...");
+			return null;
+		}
+		item = queue[front];
+		for( int i = 1; i < back; i++)
+			queue[i-1] = queue[i];
+		back--;
+		return item;
+	}
+
+	public T peekAtFront() {
+		
+		T frontItem = null;
+		
+		if( isEmpty()) {
+			System.out.println( "Queue is empty.");
+			return frontItem;
+		}
+		frontItem = queue[front];
+		
+		return frontItem;
+	}
+
+	public T peekAtBack() {
+
+		T backItem = null;
+		
+		if( isEmpty()) {
+			System.out.println( "Queue is empty.");
+			return backItem;
+		}
+		backItem = queue[back-1];
+		
+		return backItem;
+	}
+	
+	public String toString() {
+		
+		String str = "[ ";
+		
+		if( !isEmpty())
+			for( int index = 0 ; index < back ; index++ )
+				str = str + queue[index] + " ";
+		
+		str += "]";
+		
+		return str;
+	}
+
+	public int size() {
+		
+		return( back-1);
+	}
+	
+	public boolean isFull() {
+		
+		return( back == queue.length);
+	}
+
+	public boolean isEmpty() {
+		
+		return( back == 0);
+	}
+}
--- Sieve/src/Sieve.java
+++ Sieve/src/Sieve.java
@@ -1,8 +1,123 @@
+import java.util.Scanner;
 
 public class Sieve {
 
+	static Scanner scan = new Scanner (System.in);
 	public static void main(String[] args) {
-		System.out.println("test");
+		
+		int num;
+		boolean again = true;
+		
+		while (again) {
+			num = getNumber();
+			
+			CQueue<Integer> queue = new CQueue<Integer>();
+			CQueue<Integer> tempQueue = new CQueue<Integer>();
+			ABQueue<Integer> prime = new ABQueue<Integer>();
+			int temp;
+
+
+			int i = 2;
+			
+			while (i <= num) {
+				queue.enqueue(i);
+//				prime.enqueue(i);
+				i++;
+			}
+
+			
+			while (!queue.isEmpty()) {
+				tempQueue = new CQueue<Integer>();
+				
+			}
+//			i = (int) Math.sqrt(queue.peekAtBack());
+//            do {            
+//            	temp = numList.dequeue();
+// 
+//            	if (num != null){
+//            		primes.enqueue(num);
+//             
+//            		for (int i = 0; i < numList.size(); i++){
+//            			Integer num2 = numList.dequeue();
+//            			if (num2 % num != 0){
+//                            temp.enqueue(num2);
+//                        }
+//                    }
+//            		numList = temp;
+//                }
+//            }   while (num < Math.sqrt(num));
+
+			System.out.println(queue.toString());
+
+			display(prime);
+
+			again = runAgain();
+		}
+		scan.close();
 	}
 
+	public static int getNumber() {
+		int num = 0;
+		boolean again = true;
+
+		while (again) {
+			System.out.print("Please enter a number between 2 and infinitity:");
+			try {
+				num = scan.nextInt();
+				System.out.println();
+				if (num >= 2) {
+					scan.nextLine();
+					return num;
+				} else {
+					System.out.println("I'm sorry, the number you entered was invalid. Please try again.");
+				}
+			} catch (Exception InputMismatchException) {
+				System.out.println();
+				System.out.println("I'm sorry, what you entered was not a valid number. Please try again.");
+				scan.next();
+			}
+
+		}
+		return num;
+	}
+
+	public static boolean runAgain() {
+		String response;
+		boolean again = true;
+
+		while (again) {
+			System.out.print("Would you like to run again? (Y or N):");
+			response = scan.nextLine();
+			response = response.toUpperCase();
+			if (response.charAt(0) == 'Y') {
+				return true;
+			} else if (response.charAt(0) == 'N') {
+				return false;
+			}
+			System.out.println("I'm sorry, that was not a valid input. Please answer Y or N.");
+		}
+		return false;
+	}
+
+	public static void display(ABQueue queue) {
+		int num = queue.size() / 10;
+		int extra = (queue.size() % 10) + 1;
+		int i = 0;
+		int k = 0;
+
+		while (i < num) {
+			for (int f = 1; f <= 10; f++)
+				System.out.print(queue.dequeue() + " ");
+			System.out.println("");
+			i++;
+		}
+
+		while (k < extra) {
+			System.out.print(queue.dequeue() + " ");
+			k++;
+		}
+
+		System.out.println("");
+
+	}
 }
