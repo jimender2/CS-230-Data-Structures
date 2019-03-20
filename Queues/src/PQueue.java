392564c59943bc7ed8a2892ada172ccfa6696413
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2019-03-20 08:11:49 -0400

    clean up spaces and tabs

--- Queues/src/PQueue.java
+++ Queues/src/PQueue.java
@@ -2,29 +2,29 @@
 
 public class PQueue<T extends Comparable<T>> {
 
-	private class Node<T> {	
+	private class Node<T> {
 		private T data;
 		private Node<T> next;
-		
+
 		public Node( T data) {
 			this.data = data;
 			this.next = null;
 		}
 	}
-	
+
 	private Node<T> front;
 	private Node<T> back;
 	int nodeCount;
-	
+
 	public PQueue() {
-		
+
 		front = null;
 		back = null;
 		nodeCount = 0;
 	}
-	
+
 	public PQueue( PQueue<T> otherQueue) {
-		
+
 		if( otherQueue.isEmpty()) {
 			System.out.println( "Incoming queue is empty");
 		}
@@ -32,7 +32,7 @@ public class PQueue<T extends Comparable<T>> {
 			this.front = null;
 			this.back = null;
 			this.nodeCount = 0;
-			
+
 			T dataValue;
 			Node<T> currentNode = otherQueue.front;
 			while( currentNode != null) {
@@ -42,34 +42,34 @@ public class PQueue<T extends Comparable<T>> {
 			}
 		}
 	}
-	
+
 	public void enqueue( T dataValue) {
-		
+
 		Node<T> newNode = new Node<T>( dataValue);
-		
+
 		if( isEmpty())
 			front = back = newNode;
 		else {
 			boolean found = false;
 			Node<T> currentNode = front;
 			Node<T> trailCurrentNode = front;
-			
-			
+
+
 		}
-		
+
 		nodeCount++;
 		return;
 	}
-	
+
 	public T dequeue() {
-		
+
 		T node = null;
-		
+
 		if( isEmpty()) {
 			System.out.println( "Queue is empty.");
 			return node;
 		}
-		
+
 		if( front == back) {
 			node = front.data;
 			front = back = null;
@@ -78,36 +78,36 @@ public class PQueue<T extends Comparable<T>> {
 			node = front.data;
 			front = front.next;
 		}
-		
+
 		nodeCount--;
 		return node;
 	}
-		
+
 	public boolean isEmpty() {
-		
+
 		return( front == null);
 	}
-	
+
 	public int size() {
-		
+
 		return nodeCount;
 	}
-	
+
 	public String toString() {
-		
+
 		String str = "";
-		
+
 		if( isEmpty()) {
 			str = str + "Queue is empty...";
 			return str;
 		}
-		
+
 		Node<T> currentNode = front;	
 		while( currentNode != null) {
 			str = str + currentNode.data + "  ";
 			currentNode = currentNode.next;
 		}
-		
+
 		return str;
 	}
 }
