6cfbd9a45d37cd33a25f7453a7969d32c38d43ff
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2019-04-05 08:09:55 -0400

    Remove extra spaces in Large Integer Class

--- Lists/src/LargeInteger.java
+++ Lists/src/LargeInteger.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class LargeInteger {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		String line;
 		int size1, size2;
 		LinkedList<Byte> sum = new LinkedList<Byte>();
 		LinkedList<Byte> largeInt1 = new LinkedList<Byte>();
 		LinkedList<Byte> largeInt2 = new LinkedList<Byte>();
 		Scanner inFile = new Scanner( new File( "largeIntegers.txt"));
-		
+
 		line = inFile.nextLine();
 		largeInt1 = buildLargeInteger( line);
-		line = inFile.nextLine(); 	
+		line = inFile.nextLine();
 		largeInt2 = buildLargeInteger( line);
 		inFile.close();
-		
+
 		size1 = largeInt1.size();
 		size2 = largeInt2.size();
-		
+
 		System.out.println( largeInt1);
 		System.out.println( largeInt2);
 		largeInt1.reverse();
@@ -30,27 +30,27 @@ public class LargeInteger {
 		System.out.println( sum);
 		return;
 	}
-	
+
 	public static LinkedList<Byte> buildLargeInteger( String line) {
-		
+
 		Byte x;
 		String str;
 		int length = line.length();
 		LinkedList<Byte> largeInt = new LinkedList<Byte>();
-		
-		
-		
+
+
+
 		return largeInt;
-	} 
+	}
 
 	public static LinkedList<Byte> add( LinkedList<Byte> num1, LinkedList<Byte> num2) {
-		
+
 		int size = num1.size();
 		Byte x, y, z, carry = 0;
 		LinkedList<Byte> largeInt = new LinkedList<Byte>();
-		
-		
+
+
 		return largeInt;
 	}
-		
+
 }
