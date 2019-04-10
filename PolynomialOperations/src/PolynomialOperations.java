4e13515eb72fbeb04b232f81dc9f6b467cd12580
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-04-10 19:34:45 -0400

    Got polynomial Operations working

--- PolynomialOperations/src/PolynomialOperations.java
+++ PolynomialOperations/src/PolynomialOperations.java
@@ -1,31 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class PolynomialOperations {
 
-	//	public static void main(String[] args) throws FileNotFoundException {
-	//
-	//		File fileIn = new File("polynomials.txt");
-	//		Scanner file = new Scanner(fileIn);
-	//
-	//		ABList poly1 = new ABList();
-	//		ABList poly2 = new ABList();
-	//		int x;
-	//		String read;
-	//		String[] numberStrs;
-	//
-	//		x = file.nextInt();
-	//
-	//		read = file.nextLine();
-	//
-	//		numberStrs = read.split(" ");
-	//		int[] numbers = new int[numberStrs.length];
-	//		for(int i = 0; i < numberStrs.length; i++)
-	//		{
-	//			numbers[i] = Integer.parseInt(numberStrs[i]);
-	//		}
-
-
 	// Java program to add two polynomials
-
-
 
 	// A utility function to return maximum of two integers
 	static int max(int m, int n) {
@@ -117,35 +96,83 @@ public class PolynomialOperations {
 
 
 	// Driver program to test above functions
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		//
+		File fileIn = new File("polynomials.txt");
+		Scanner file = new Scanner(fileIn);
+		//
+		int x;
+		String read;
+		String[] numberStrs;
 
-		ABList<Integer> poly1 = new ABList<Integer>();
+		x = file.nextInt();
+
+		file.nextLine();
+
+		read = file.nextLine();
+		ABList<Integer> poly1 = new ABList<>();
+
+		int exponent = 0;
+		int coefficient = 0;
+		while (exponent < read.length() - 1) {
+			exponent = coefficient + 2;
+			poly1.insert(read.charAt(coefficient) - 48,
+					read.charAt(exponent) - 48);
+			coefficient = exponent + 2;
+		}
+
+
+		read = file.nextLine();
 		ABList<Integer> poly2 = new ABList<Integer>();
+		exponent = 0;
+		coefficient = 0;
+		while (exponent < read.length() - 1) {
+			exponent = coefficient + 2;
+			poly2.insert(read.charAt(coefficient) - 48,
+					read.charAt(exponent) - 48);
+			coefficient = exponent + 2;
+		}
 
-		poly1.insert(Integer.parseInt("5"));
-		poly1.insert(Integer.parseInt("0"));
-		poly1.insert(Integer.parseInt("10"));
-		poly1.insert(Integer.parseInt("-6"));
-
-		poly2.insert(Integer.parseInt("1"));
-		poly2.insert(Integer.parseInt("2"));
-		poly2.insert(Integer.parseInt("4"));
-
-		int x = -1;
-		// The following array represents polynomial 5 + 10x^2 + 6x^3
-		int A[] = {5, 0, 10, 6};
-
-		// The following array represents polynomial 1 + 2x + 4x^2
-		int B[] = {1, 2, 4};
-
-		System.out.println("First polynomial is");
+		System.out.println("Test");
 		printPolyABList(poly1);
-		System.out.println("\nSecond polynomial is");
+		System.out.println();
+		System.out.println("Test2");
 		printPolyABList(poly2);
 
 		System.out.println();
 		System.out.println();
 
+		System.out.println("poly1: " + poly1);
+		System.out.println("poly2 : " + poly2);
+
+
+		//		ABList<Integer> poly1 = new ABList<Integer>();
+		//		ABList<Integer> poly2 = new ABList<Integer>();
+		//
+		//		poly1.insert(Integer.parseInt("5"));
+		//		poly1.insert(Integer.parseInt("0"));
+		//		poly1.insert(Integer.parseInt("10"));
+		//		poly1.insert(Integer.parseInt("-6"));
+		//
+		//		poly2.insert(Integer.parseInt("1"));
+		//		poly2.insert(Integer.parseInt("2"));
+		//		poly2.insert(Integer.parseInt("4"));
+		//
+		//		// The following array represents polynomial 5 + 10x^2 + 6x^3
+		//		int A[] = {5, 0, 10, 6};
+		//
+		//		// The following array represents polynomial 1 + 2x + 4x^2
+		//		int B[] = {1, 2, 4};
+		//
+		System.out.println("First polynomial is");
+		printPolyABList(poly1);
+		System.out.println("\nSecond polynomial is");
+		printPolyABList(poly2);
+		//
+		System.out.println();
+		System.out.println();
+		//
+		System.out.println("Subtract");
 		ABList<Integer> polySub = subtractPoly(poly1, poly2);
 		printPolyABList(polySub);
 
