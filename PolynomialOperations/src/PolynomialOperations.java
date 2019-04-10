3ccc5ae8c42824c2569e7ba40279f2a1e5e564ff
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-04-09 22:26:25 -0400

    add polynomials.txt

--- PolynomialOperations/src/PolynomialOperations.java
+++ PolynomialOperations/src/PolynomialOperations.java
@@ -1,4 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class PolynomialOperations {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		File fileIn = new File("polynomials.txt");
+		Scanner file = new Scanner(fileIn);
+	}
 
 }
