2eca5fd59303bb8ba2ef23dd9de101453aa66135
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-04-24 10:05:10 -0400

    Finished Spell check

--- SpellCheck/src/SpellCheck.java
+++ SpellCheck/src/SpellCheck.java
@@ -5,16 +5,29 @@ import java.util.Scanner;
 public class SpellCheck {
 
 	public static void main(String args[]) throws FileNotFoundException {
-		BST<String> dictionary = new BST<String>();
+		BST<String> dictionary = new BST<>();
 		Scanner file = new Scanner(new File ("words.txt"));
 		while (file.hasNext()) {
 			dictionary.insert(file.nextLine());
 		}
+		file.close();
 
-		System.out.println("Done");
-		System.out.println(dictionary.getLeafNodeCount());
-		System.out.println(dictionary.getTreeHeight());
-		System.out.println(dictionary.getNodeCount());
-		dictionary.inOrderTraversal();
+		String temp = "";
+		BST<String> misspelled = new BST<>();
+
+		Scanner scan = new Scanner(new File ("sampleText.txt"));
+		while (scan.hasNext()) {
+			temp = scan.nextLine().replaceAll("[.,]", "");
+			String array [] = temp.split(" ");
+			for(int i = 0; i < array.length; i++)
+				if( !dictionary.searchFor(array[i]))
+					misspelled.insert(array[i]);
+		}
+		scan.close();
+
+		if (misspelled.isEmpty())
+			System.out.println("There are no misspelled words");
+		else
+			dictionary.inOrderTraversal();
 	}
 }
